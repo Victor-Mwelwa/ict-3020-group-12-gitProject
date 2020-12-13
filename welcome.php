f7301a516db647b41e8ad04b061442ae9d59d5e4
Victor-Mwelwa <2017013155@student.unza.zm>
2020-12-13 14:47:12 -0800

    added mark.php as well as welcome.php and some bootstrap

---
+++ welcome.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$username = "";
+$password ="";
+$email = "";
+$course = "";
+$computerNumber = "";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+    <title>UNZA TAR REGISTRATION</title>
+
+  </head>
+  <body>
+
+      <div class="wrapper">
+
+          <h3> WELCOME TO UNZA TAR </h3>
+          <h2><a href="mark.php">mark register </a>.<br>
+          </2>
+              
+          <h4><a href="mark.php">view register </a>.<br>
+          </h42>
+
+
+
+
+
+            </form>
+        </div>
+
+  </body>
+</html>
